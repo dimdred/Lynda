e0b291855f92cda64d8e0b4609ffa378dd61aa62
Dzmitry_Dashkevich <Dzmitry_Dashkevich@epam.com>
2017-06-20 14:57:28 +0300

    cond

--- Learning python/conditional.py
+++ Learning python/conditional.py
@@ -2,13 +2,13 @@ def main():
     x, y = 10, 100
 
 # conditional flow uses if, elif, else
-#     if(x < y):
-#         st = "x is less than y"
-#     elif(x == y):
-#         st = "x is same as y"
-#     else:
-#         st = "x is greater than y"
-#     print st
+    if(x < y):
+        st = "x is less than y"
+    elif(x == y):
+        st = "x is same as y"
+    else:
+        st = "x is greater than y"
+    print st
 
 
 #conditional statements let you use "A if C else B"
