f1deec195aa0ec8455a3d8b94be1496f63197780
Dzmitry_Dashkevich <Dzmitry_Dashkevich@epam.com>
2017-07-20 15:41:41 +0300

    paths, time_modify

---
+++ Learning python/OS path.py
@@ -0,0 +1,27 @@
+import os
+from os import path
+import datetime
+from datetime import date, time, timedelta
+import time
+
+# print name of OS
+print os.name
+
+# check for item existence and type
+print "Item exist: " + str(path.exists("textfile.txt"))
+print "Item is a file: " + str(path.isfile("textfile.txt"))
+print "Item is a directory: " + str(path.isdir("textfile.txt"))
+
+# work with real paths
+print "Item`s paths: " + str(path.realpath("textfile.txt"))
+print "Item`s paths and name: " + str(path.split(path.realpath("textfile.txt")))
+
+# get the modification time
+t = time.ctime(path.getmtime("textfile.txt"))
+print t
+print datetime.datetime.fromtimestamp(path.getmtime("textfile.txt"))
+
+# calculate how long ago the item was modified
+td = datetime.datetime.now() - datetime.datetime.fromtimestamp(path.getmtime("textfile.txt"))
+print "It has been " + str(td) + "The file was modified"
+print "Or," + str(td.total_seconds()) + "seconds"
